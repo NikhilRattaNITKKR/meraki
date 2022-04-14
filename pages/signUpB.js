678009db67b8c92b3be366c3506dc50cd62d8022
Faisal <faisalalik9@gmail.com>
2022-04-14 21:47:46 +0530

    Resructured

---
+++ pages/signUpB.js
@@ -0,0 +1,20 @@
+import SignUpBorrower from "../components/SignUpBorrower";
+
+const signUpB = () => {
+    return (
+        <div className=''>
+
+
+          <div className='px-4 py-2 lg:px-12 lg:py-4 mt-12  '>
+
+            <SignUpBorrower />
+
+          </div>
+
+
+
+        </div>
+    );
+}
+
+export default signUpB;
